5ab35726cbfafa21d2bf9a2d58d3f207e39b1a73
YuanWing <yuan_wing@163.com>
2017-02-12 10:31:53 +0800

    50间图

---
+++ Japanese/50.js
@@ -0,0 +1,217 @@
+var yin = [
+  [{
+    hiragana: 'あ',
+    katakana: 'ア',
+    roman: 'a',
+  }, {
+    hiragana: 'い',
+    katakana: 'イ',
+    roman: 'i',
+  }, {
+    hiragana: 'う',
+    katakana: 'ウ',
+    roman: 'u',
+  }, {
+    hiragana: 'え',
+    katakana: 'エ',
+    roman: 'e',
+  }, {
+    hiragana: 'お',
+    katakana: 'オ',
+    roman: 'o',
+  }],
+  [{
+    hiragana: 'か',
+    katakana: 'カ',
+    roman: 'ka',
+  }, {
+    hiragana: 'き',
+    katakana: 'キ',
+    roman: 'ki',
+  }, {
+    hiragana: 'く',
+    katakana: 'ク',
+    roman: 'ku',
+  }, {
+    hiragana: 'け',
+    katakana: 'ケ',
+    roman: 'ke',
+  }, {
+    hiragana: 'こ',
+    katakana: 'コ',
+    roman: 'ko',
+  }],
+  [{
+    hiragana: 'さ',
+    katakana: 'サ',
+    roman: 'sa',
+  }, {
+    hiragana: 'し',
+    katakana: 'シ',
+    roman: 'shi',
+  }, {
+    hiragana: 'す',
+    katakana: 'ス',
+    roman: 'su',
+  }, {
+    hiragana: 'せ',
+    katakana: 'セ',
+    roman: 'se',
+  }, {
+    hiragana: 'そ',
+    katakana: 'ソ',
+    roman: 'so',
+  }],
+  [{
+    hiragana: 'た',
+    katakana: 'タ',
+    roman: 'ta',
+  }, {
+    hiragana: 'ち',
+    katakana: 'チ',
+    roman: 'chi',
+  }, {
+    hiragana: 'つ',
+    katakana: 'ツ',
+    roman: 'tsu',
+  }, {
+    hiragana: 'て',
+    katakana: 'テ',
+    roman: 'te',
+  }, {
+    hiragana: 'と',
+    katakana: 'ト',
+    roman: 'to',
+  }],
+  [{
+    hiragana: 'な',
+    katakana: 'ナ',
+    roman: 'na',
+  }, {
+    hiragana: 'に',
+    katakana: 'ニ',
+    roman: 'ni',
+  }, {
+    hiragana: 'ぬ',
+    katakana: 'ヌ',
+    roman: 'nu',
+  }, {
+    hiragana: 'ね',
+    katakana: 'ネ',
+    roman: 'ne',
+  }, {
+    hiragana: 'の',
+    katakana: 'ノ',
+    roman: 'no',
+  }],
+  [{
+    hiragana: 'は',
+    katakana: 'ハ',
+    roman: 'ha',
+  }, {
+    hiragana: 'ひ',
+    katakana: 'ヒ',
+    roman: 'hi',
+  }, {
+    hiragana: 'ふ',
+    katakana: 'フ',
+    roman: 'fu',
+  }, {
+    hiragana: 'へ',
+    katakana: 'ヘ',
+    roman: 'he',
+  }, {
+    hiragana: 'ほ',
+    katakana: 'ホ',
+    roman: 'ho',
+  }],
+  [{
+    hiragana: 'ま',
+    katakana: 'マ',
+    roman: 'ma',
+  }, {
+    hiragana: 'み',
+    katakana: 'ミ',
+    roman: 'mi',
+  }, {
+    hiragana: 'む',
+    katakana: 'ム',
+    roman: 'mu',
+  }, {
+    hiragana: 'め',
+    katakana: 'メ',
+    roman: 'me',
+  }, {
+    hiragana: 'も',
+    katakana: 'モ',
+    roman: 'mo',
+  }],
+  [{
+    hiragana: 'や',
+    katakana: 'ヤ',
+    roman: 'ya',
+  }, {
+    hiragana: 'い',
+    katakana: 'イ',
+    roman: 'i',
+  }, {
+    hiragana: 'ゆ',
+    katakana: 'ユ',
+    roman: 'yu',
+  }, {
+    hiragana: 'え',
+    katakana: 'エ',
+    roman: 'e',
+  }, {
+    hiragana: 'よ',
+    katakana: 'ヨ',
+    roman: 'yo',
+  }],
+  [{
+    hiragana: 'ら',
+    katakana: 'ラ',
+    roman: 'ra',
+  }, {
+    hiragana: 'れ',
+    katakana: 'レ',
+    roman: 'ri',
+  }, {
+    hiragana: 'る',
+    katakana: 'ル',
+    roman: 'ru',
+  }, {
+    hiragana: 'れ',
+    katakana: 'レ',
+    roman: 're',
+  }, {
+    hiragana: 'ろ',
+    katakana: 'ロ',
+    roman: 'ro',
+  }],
+  [{
+    hiragana: 'わ',
+    katakana: 'ワ',
+    roman: 'wa',
+  }, {
+    hiragana: 'い',
+    katakana: 'イ',
+    roman: 'i',
+  }, {
+    hiragana: 'う',
+    katakana: 'ウ',
+    roman: 'u',
+  }, {
+    hiragana: 'え',
+    katakana: 'エ',
+    roman: 'e',
+  }, {
+    hiragana: 'を',
+    katakana: 'ヲ',
+    roman: 'o',
+  }],
+  [{
+    hiragana: 'ん',
+    katakana: 'ン',
+    roman: 'n',
+  }],
+];
